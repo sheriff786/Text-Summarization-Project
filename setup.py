7de065785e9422c17c82f1b68de3376ecd90c675
Mohd Sheriff Mehmood <modsheriff27021996@gmail.com>
2023-08-08 23:03:36 +0530

    requirements & installation

---
+++ setup.py
@@ -0,0 +1,33 @@
+#we used setup.py for 
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarization-Project"
+AUTHOR_USER_NAME = "sheriff786"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "mohdsheriff27021996@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src"),
+)
+
+
+
+
+
+
